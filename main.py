2e9d3c7a45a0bb9e31ab4ba5fde966cc10e5ed9f
Gector-Lore <168639156+Gector-Lore@users.noreply.github.com>
2024-06-08 15:19:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+result1 = fm(34, 2)
+result2 = fm(26, 0)
+result3 = tm(62, 3)
+result4 = tm(76,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
